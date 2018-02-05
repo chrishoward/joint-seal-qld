071f1f28f036bd35ac357f502c7caaf3d0c1043f
chris <chrisau233@gmail.com>
2018-02-05 23:48:59 +1000

    new file creation

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php
+    // if (isset($_POST['name'])) {
+    //     echo "Test";
+    // }
+
+    echo "hello world";
+?>
+
+Success
+
+<?php
+    // if (isset($_POST['name'])) {
+    //     echo "Test";
+    // }
+
+    echo "hello world";
+?>
