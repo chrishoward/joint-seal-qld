feabe49e19df45aec6226ff1d37883896bcf7ec2
chris <chrisau233@gmail.com>
2018-02-09 21:02:57 +1000

    minor image-slider changes - styling and autoplay

--- js/slick-image-slider-init.js
+++ js/slick-image-slider-init.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-    $('.single-item').slick();
+    $('.single-item').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        // dots: true
+        responsive: [
+            {
+                breakpoint: 740,
+                settings: {
+                    arrows: false,
+                }
+            },
+        ]
+    });
 });
