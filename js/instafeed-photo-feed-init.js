18e796b7bfcff1deae7c285909da1a82eb09d604
chris <chrisau233@gmail.com>
2018-02-13 23:55:46 +1000

    javscript files added for Instagram photo feed

---
+++ js/instafeed-photo-feed-init.js
@@ -0,0 +1,13 @@
+var userFeed = new Instafeed({
+    get: 'user',
+    userId: '7101810308',
+    accessToken: '7101810308.fefcdfa.01da190d9af54e5599a86dce9d8f9b25'
+});
+userFeed.run();
+
+// var feed = new Instafeed({
+//     get: 'tagged',
+//     tagName: 'awesome',
+//     clientId: '782166caa35740aab1a274ac06f60f86'
+// });
+// feed.run();
