32b180dbb950b5366d1ba41df7b9a35d691da1fb
chris <chrisau233@gmail.com>
2018-02-21 22:40:18 +1000

    minor formatting

--- js/map-polygon.js
+++ js/map-polygon.js
@@ -2,15 +2,15 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById('js-map'), {
     zoom: 8,
-    center: {lat: -27.397139, lng: 152.783694},
+    center: { lat: -27.397139, lng: 152.783694 },
     mapTypeId: 'roadmap'
   });
 
   // Define the LatLng coordinates for the polygon's path.
   var serviceAreaCoords = [
-    {lat: -26.3980, lng: 153.0930},
-    {lat: -27.56433, lng: 151.953987},
-    {lat: -28.178666, lng: 153.537001},
+    { lat: -26.3980, lng: 153.0930 },
+    { lat: -27.56433, lng: 151.953987 },
+    { lat: -28.178666, lng: 153.537001 },
   ];
 
   // Construct the polygon.
